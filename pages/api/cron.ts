a1e51bb1bc0a7b9203e8f4da6411e6d670da6691
gtg7784 <tae.gun7784@gmail.com>
2023-03-16 16:34:23 +0900

    feat(api): `cron` api 추가

--- pages/api/cron.ts
+++ pages/api/cron.ts
@@ -1,12 +1,51 @@
 import type { NextApiRequest, NextApiResponse } from 'next'
+import RssParser from 'rss-parser';
+import redis from '@/utils/redis'
+import { send_messege } from '@/utils/telegram';
 
-type Data = {
-  name: string
-}
-
-export default function handler(
+export default async function handler(
   req: NextApiRequest,
-  res: NextApiResponse<Data>
+  res: NextApiResponse<unknown>
 ) {
+  const rss_list = [
+    'https://www.chosun.com/arc/outboundfeeds/rss/?outputType=xml',
+    'https://rss.donga.com/total.xml',
+    'https://rss.hankyung.com/feed/headline.xml'
+  ];
+
+  rss_list.forEach(async (rss_url) => {
+    const parser = new RssParser();
+    const feed = await parser.parseURL(rss_url);
+
+    const press = feed.title;
+    const last_article = feed.items[0].title;
+    const last_article_link = feed.items[0].link;
+
+    const last_article_key = `${press}_last_article_title`;
+    const last_articel_link_key = `${press}_last_article_link`;
+
+    const last_article_in_redis = await redis.get(last_article_key);
+    const last_article_link_in_redis = await redis.get(last_articel_link_key);
+
+    const is_duplicated = 
+      last_article_in_redis === last_article ||
+      last_article_link_in_redis === last_article_link;
+
+    if (is_duplicated) {
+      return;
+    }
+
+    await redis.set(last_article_key, last_article);
+    await redis.set(last_articel_link_key, last_article_link);
+
+    const chat_id = process.env.TELEGRAM_CHAT_ID as string;
+    const messege = `[${press}] ${last_article}\n${last_article_link}`;
+
+    send_messege(
+      chat_id,
+      messege
+    );
+  });
+
   res.status(200).json({ name: 'John Doe' })
 }
